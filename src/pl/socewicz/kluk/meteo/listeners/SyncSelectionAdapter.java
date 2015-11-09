f56efb0bdf4197926ea1268122e2b7c48d31ee3a
Waldemar Kluk <waldemarkluk@gmail.com>
2015-11-09 04:30:56 +0100

    pogodynka + ekologia + strona z pomiarami
    Trzeba dodać pobranie pliku graficznego + parsing!

--- src/pl/socewicz/kluk/meteo/listeners/SyncSelectionAdapter.java
+++ src/pl/socewicz/kluk/meteo/listeners/SyncSelectionAdapter.java
@@ -16,6 +16,7 @@ import org.xml.sax.SAXException;
 
 import pl.socewicz.kluk.util.HTMLParserEkologia;
 import pl.socewicz.kluk.util.HTMLParserPogodynka;
+import pl.socewicz.kluk.util.HTMLParserPomiary;
 
 public class SyncSelectionAdapter extends SelectionAdapter {
 	StyledText log;
@@ -35,6 +36,12 @@ public class SyncSelectionAdapter extends SelectionAdapter {
 	}
 	
 	private void downloadData() throws SAXException, IOException, URISyntaxException, ParserConfigurationException{
+		try {
+			new HTMLParserPomiary();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		new HTMLParserPogodynka();
 		new HTMLParserEkologia();
 	}
